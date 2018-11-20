4002425a4ebbe99cf586d9d6b46840df1aed5520
hongyu <yuhongyu04@126.com>
2018-11-21 03:39:16 +0800

    add more test code for core actors

--- actors/src/main/scala/org/loopring/lightcone/actors/data/package.scala
+++ actors/src/main/scala/org/loopring/lightcone/actors/data/package.scala
@@ -16,26 +16,87 @@
 
 package org.loopring.lightcone.actors
 
-import com.google.protobuf.ByteString
-import org.loopring.lightcone.core.data.Order
+import org.loopring.lightcone.core.data.{ Order, OrderState }
 import org.loopring.lightcone.proto.actors._
 import com.google.protobuf.ByteString
 
-// TODO(hongyu): implement this
 package object data {
 
   case class BalanceAndAllowance(balance: BigInt, allowance: BigInt)
 
   ///////////
 
-  implicit def byteString2BigInt(bytes: ByteString): BigInt = ???
-  implicit def bigInt2ByteString(b: BigInt): ByteString = ???
+  implicit def byteString2BigInt(bytes: ByteString): BigInt = {
+    if (bytes.size() > 0)
+      BigInt(bytes.toByteArray)
+    else BigInt(0)
+  }
+  implicit def bigInt2ByteString(b: BigInt): ByteString = ByteString.copyFrom(b.toByteArray)
 
-  implicit def xBalanceAndAlowance2BalanceAndAlowance(ba: XBalanceAndAllowance): BalanceAndAllowance = ???
-  implicit def balanceAndAlowance2XBalanceAndAlowance(ba: BalanceAndAllowance): XBalanceAndAllowance = ???
+  implicit def xBalanceAndAlowance2BalanceAndAlowance(xba: XBalanceAndAllowance): BalanceAndAllowance = {
+    BalanceAndAllowance(
+      balance = xba.balance,
+      allowance = xba.allowance
+    )
+  }
+  implicit def balanceAndAlowance2XBalanceAndAlowance(ba: BalanceAndAllowance): XBalanceAndAllowance = {
+    XBalanceAndAllowance(
+      balance = ba.balance,
+      allowance = ba.allowance
+    )
+  }
 
-  implicit def xOrder2Order(xorder: XOrder): Order = ???
-  implicit def order2XOrder(order: Order): XOrder = ???
+  implicit def xOrder2Order(xorder: XOrder): Order = {
+    Order(
+      id = xorder.id,
+      tokenS = xorder.tokenS,
+      tokenB = xorder.tokenB,
+      tokenFee = xorder.tokenFee,
+      amountS = xorder.amountS,
+      amountB = xorder.amountB,
+      amountFee = xorder.amountFee,
+      createdAt = xorder.createdAt,
+      status = xorder.status,
+      walletSplitPercentage = xorder.walletSplitPercentage,
+      _outstanding = Some(xorder.getOutstanding),
+      _reserved = Some(xorder.getReserved),
+      _actual = Some(xorder.getActual),
+      _matchable = Some(xorder.getMatchable)
+    )
+  }
+  implicit def order2XOrder(order: Order): XOrder = {
+    XOrder(
+      id = order.id,
+      tokenS = order.tokenS,
+      tokenB = order.tokenB,
+      tokenFee = order.tokenFee,
+      amountS = order.amountS,
+      amountB = order.amountB,
+      amountFee = order.amountFee,
+      createdAt = order.createdAt,
+      status = order.status,
+      walletSplitPercentage = order.walletSplitPercentage,
+      outstanding = Some(order.outstanding),
+      reserved = Some(order.reserved),
+      actual = Some(order.actual),
+      matchable = Some(order.matchable)
+    )
+  }
+
+  implicit def XOrderStateToOrderState(xOrderState: XOrderState): OrderState = {
+    OrderState(
+      amountS = xOrderState.amountS,
+      amountB = xOrderState.amountB,
+      amountFee = xOrderState.amountFee
+    )
+  }
+  implicit def OrderStateToXOrderState(orderState: OrderState): XOrderState = {
+    XOrderState(
+      amountS = orderState.amountS,
+      amountB = orderState.amountB,
+      amountFee = orderState.amountFee
+    )
+  }
 
   implicit def byteArray2ByteString(bytes: Array[Byte]) = ByteString.copyFrom(bytes)
 }
--- actors/src/test/scala/org/loopgring/lightcone/actors/core/CoreActorsIntegrationSpec_Simple.scala
+++ actors/src/test/scala/org/loopgring/lightcone/actors/core/CoreActorsIntegrationSpec_Simple.scala
@@ -16,30 +16,105 @@
 
 package org.loopgring.lightcone.actors.core
 
-import akka.actor._
-import akka.testkit._
-import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
-import org.loopring.lightcone.actors.core._
-import org.loopring.lightcone.core.base._
-import org.loopring.lightcone.core.depth._
-import org.loopring.lightcone.core.market._
-import org.loopring.lightcone.proto.actors.XOrder
-import org.loopring.lightcone.proto.core._
-import org.loopring.lightcone.proto.deployment._
-import org.scalatest._
+import org.loopring.lightcone.actors.data._
+import org.loopring.lightcone.proto.actors._
+import org.loopring.lightcone.proto.core.XOrderStatus
+import akka.pattern._
+import akka.testkit.TestProbe
 
 import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future }
 
 class CoreActorsIntegrationSpec_Simple
   extends CoreActorsIntegrationCommonSpec {
 
-  "submitOrder" must {
+  def setBalanceActorReply(req: Any): Future[Unit] = for {
+    _ ← Future.successful()
+  } yield {
+    accountBalanceProbe.expectUpdate(req)
+  }
 
-    "send orderUpdateEvent to orderbookManagerActor" in {
+  "submitOrder to accountManagerActor" must {
 
-      accountManagerActor ! "something"
+    "send Order to marketManagerActor" in {
 
+      setBalanceActorReply()
+      setBalanceActorReply()
+      setBalanceActorReply()
+      setBalanceActorReply()
+      setBalanceActorReply()
+      setBalanceActorReply()
+      val maker1 = XOrder(
+        id = "maker1",
+        tokenS = WETH,
+        tokenB = LRC,
+        tokenFee = LRC,
+        amountS = BigInt(100),
+        amountB = BigInt(10),
+        amountFee = BigInt(10),
+        walletSplitPercentage = 0.2,
+        status = XOrderStatus.NEW
+      )
+      val taker1 = XOrder(
+        id = "taker1",
+        tokenS = LRC,
+        tokenB = WETH,
+        tokenFee = LRC,
+        amountS = BigInt(10),
+        amountB = BigInt(100),
+        amountFee = BigInt(10),
+        walletSplitPercentage = 0.2,
+        status = XOrderStatus.NEW
+      )
+
+      accountManagerActor ! XSubmitOrderReq(Some(maker1))
+      accountManagerActor ! XSubmitOrderReq(Some(taker1))
+      Thread.sleep(10000)
+    }
+
+  }
+  "submitOrder to marketManagerActor" must {
+
+    "create ring" in {
+      var decimal = "000000000000000000000000000000".substring(0, 18)
+      val maker1 = XOrder(
+        id = "maker1",
+        tokenS = WETH,
+        tokenB = LRC,
+        tokenFee = LRC,
+        amountS = BigInt("10" + decimal),
+        amountB = BigInt("100" + decimal),
+        amountFee = BigInt("10" + decimal),
+        walletSplitPercentage = 0.2,
+        status = XOrderStatus.NEW,
+        actual = Some(XOrderState(BigInt("10" + decimal), BigInt("100" + decimal), BigInt("10" + decimal))),
+        matchable = Some(XOrderState(BigInt("10" + decimal), BigInt("100" + decimal), BigInt("10" + decimal)))
+      )
+
+      val taker1 = XOrder(
+        id = "taker1",
+        tokenS = LRC,
+        tokenB = WETH,
+        tokenFee = LRC,
+        amountS = BigInt("100" + decimal),
+        amountB = BigInt("10" + decimal),
+        amountFee = BigInt("10" + decimal),
+        walletSplitPercentage = 0.2,
+        status = XOrderStatus.NEW,
+        actual = Some(XOrderState(BigInt("100" + decimal), BigInt("10" + decimal), BigInt("10" + decimal))),
+        matchable = Some(XOrderState(BigInt("100" + decimal), BigInt("10" + decimal), BigInt("10" + decimal)))
+      )
+
+      val maker1Future = marketManagerActor ? XSubmitOrderReq(Some(maker1))
+      val taker1Future = marketManagerActor ? XSubmitOrderReq(Some(taker1))
+      val maker1Res = Await.result(maker1Future.mapTo[XSubmitOrderRes], timeout.duration)
+      val taker1Res = Await.result(taker1Future.mapTo[XSubmitOrderRes], timeout.duration)
+      info(maker1Res.error.toString())
+      info(taker1Res.error.toString())
+      val orderbookPpobe = TestProbe()
+      orderbookPpobe watch orderbookManagerActor
+      orderbookPpobe.receiveOne(1 seconds)
+      Thread.sleep(10000)
     }
 
   }
