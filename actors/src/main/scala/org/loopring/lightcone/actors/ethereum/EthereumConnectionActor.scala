1822b6bd157dd132404317c9a3c9330b7bdeacff
Daniel Wang <daniel@loopring.org>
2018-11-30 01:11:36 -0500

    rename ethrpc.proto to ethereum.proto

--- actors/src/main/scala/org/loopring/lightcone/actors/ethereum/EthereumConnectionActor.scala
+++ actors/src/main/scala/org/loopring/lightcone/actors/ethereum/EthereumConnectionActor.scala
@@ -20,7 +20,7 @@ import akka.actor._
 import akka.routing._
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
-import org.loopring.lightcone.proto.ethrpc._
+import org.loopring.lightcone.proto.ethereum._
 
 class EthereumConnectionActor(
     settings: XEthereumProxySettings
