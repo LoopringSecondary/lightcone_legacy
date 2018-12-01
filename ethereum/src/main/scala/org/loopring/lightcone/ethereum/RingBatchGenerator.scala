c835575cd76b45ff6f5557ce2c98bf644bf2e3ea
kongliangzhong <kongliangzhong@gmail.com>
2018-12-01 18:31:23 +0800

    implement RingBatchGenerator

--- ethereum/src/main/scala/org/loopring/lightcone/ethereum/RingBatchGenerator.scala
+++ ethereum/src/main/scala/org/loopring/lightcone/ethereum/RingBatchGenerator.scala
@@ -16,6 +16,8 @@
 
 package org.loopring.lightcone.ethereum.data
 
+import org.web3j.crypto.{ Hash ⇒ Web3Hash }
+import org.web3j.utils.Numeric
 import org.loopring.lightcone.proto.core._
 
 trait RingBatchGenerator {
@@ -23,7 +25,164 @@ trait RingBatchGenerator {
 }
 
 // TODO(kongliang): implement and test this class
-class RingBatchGeneratorImpl()
+class RingBatchGeneratorImpl(context: XRingBatchContext)
   extends RingBatchGenerator {
-  def generateAndSignRingBatch(orders: Seq[Seq[XRawOrder]]): XRingBatch = ???
+
+  class BytesPacker() {
+    private val ADDRESS_LENGTH = 20
+    private var data: String = ""
+
+    def getPackedString() = {
+      if (data.length.equals(0)) "0x0"
+      else "0x" + data
+    }
+
+    def getPackedBytes() = Numeric.hexStringToByteArray(data)
+    def length() = data.length / 2
+
+    def addAddress(x: String, forceAppend: Boolean = false) =
+      insert(
+        Numeric.toHexStringNoPrefixZeroPadded(
+          Numeric.toBigInt(x),
+          ADDRESS_LENGTH * 2
+        ),
+        forceAppend
+      )
+
+    def addUint16(num: BigInt, forceAppend: Boolean = true) =
+      addBigInt(num, 2, forceAppend)
+
+    def addUint32(num: BigInt, forceAppend: Boolean = true) =
+      addBigInt(num, 4, forceAppend)
+
+    def addUint(num: BigInt, forceAppend: Boolean = true) =
+      addBigInt(num, 32, forceAppend)
+
+    def addUintStr(numStr: String, forceAppend: Boolean = true) =
+      addBigIntStr(numStr, 32, forceAppend)
+
+    def addX(num: BigInt, numBytes: Int, forceAppend: Boolean = true) =
+      addBigInt(num, numBytes, forceAppend)
+
+    def addBoolean(b: Boolean, forceAppend: Boolean = true) =
+      addBigInt(if (b) 1 else 0, 1, forceAppend)
+
+    def addHex(str: String, forceAppend: Boolean = true) =
+      insert(Numeric.cleanHexPrefix(str), forceAppend)
+
+    def addRawBytes(str: String, forceAppend: Boolean = true) =
+      insert(Numeric.cleanHexPrefix(str), forceAppend)
+
+    private def padString(str: String, numBytes: Int) = {
+      val cleanStr = Numeric.cleanHexPrefix(str)
+      val targetLen = numBytes * 2
+      if (cleanStr.length > targetLen) {
+        throw new IllegalArgumentException(str + " is too long for padding")
+      }
+      val paddingLen = targetLen - cleanStr.length
+      "0" * paddingLen + cleanStr
+    }
+
+    // TODO(kongliang): 负数问题
+    private def addBigInt(
+      num: BigInt,
+      numBytes: Int,
+      forceAppend: Boolean = true
+    ) =
+      insert(
+        Numeric.toHexStringNoPrefixZeroPadded(
+          num.bigInteger,
+          numBytes * 2
+        ),
+        forceAppend
+      )
+
+    private def addBigIntStr(
+      numStr: String,
+      numBytes: Int,
+      forceAppend: Boolean = true
+    ) = {
+      insert(padString(numStr, numBytes), forceAppend)
+    }
+
+    private def insert(x: String, forceAppend: Boolean): Int = {
+      var offset = length()
+
+      if (!forceAppend) {
+        // Check if the data we're inserting is already available in the bitstream.
+        // If so, return the offset to the location.
+        var start = 0
+        while (start != -1) {
+          start = data.indexOf(x, start)
+          if (start != -1) {
+            if ((start % 2) == 0) {
+              offset = start / 2
+              return offset
+            } else {
+              start += 1
+            }
+          }
+        }
+      }
+
+      data ++= x
+      offset
+    }
+  }
+
+  object OrderHelper {
+    def calculateHash(order: XRawOrder) = {
+      val packer = new BytesPacker
+      packer.addUintStr(order.amountS.toString)
+      packer.addUintStr(order.amountB.toString)
+      packer.addUintStr(order.feeAmount.toString)
+      packer.addUint(BigInt(order.validSince))
+      packer.addUint(BigInt(order.validUntil))
+      packer.addAddress(order.owner, true)
+      packer.addAddress(order.tokenS, true)
+      packer.addAddress(order.tokenB, true)
+      packer.addAddress(order.dualAuthAddress, true)
+      packer.addAddress(order.broker, true)
+      packer.addAddress(order.orderInterceptor, true)
+      packer.addAddress(order.wallet, true)
+      packer.addAddress(order.tokenRecipient, true)
+      packer.addAddress(order.feeToken, true)
+      packer.addUint16(order.walletSplitPercentage)
+      packer.addUint16(order.tokenSFeePercentage)
+      packer.addUint16(order.tokenBFeePercentage)
+      packer.addBoolean(order.allOrNone)
+
+      order.copy(hash = Numeric.toHexString(Web3Hash.sha3(packer.getPackedBytes)))
+    }
+
+    def sign(order: XRawOrder) = {
+      order.copy()
+    }
+  }
+
+  def sign(xRingBatch: XRingBatch) = {
+    xRingBatch
+  }
+
+  def generateAndSignRingBatch(orders: Seq[Seq[XRawOrder]]): XRingBatch = {
+    val ordersDistinctedMap = orders
+      .flatten
+      .map(OrderHelper.calculateHash)
+      .map(o ⇒ o.hash -> o)
+      .toMap
+
+    val ordersDistinctedSeq = ordersDistinctedMap
+      .map(_._2)
+      .map(OrderHelper.sign)
+
+    val ordersHashIndexMap = ordersDistinctedSeq
+      .zipWithIndex
+      .toMap
+
+    val xringBatch = new XRingBatch()
+      .withFeeRecipient(context.feeRecipient)
+      .withMiner(context.miner)
+
+    sign(xringBatch)
+  }
 }
