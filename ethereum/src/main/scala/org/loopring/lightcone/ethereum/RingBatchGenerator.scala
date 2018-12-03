04afa6220add9a246fe13b0deec1ceb538a640ac
kongliangzhong <kongliangzhong@gmail.com>
2018-12-03 18:29:41 +0800

    update

--- ethereum/src/main/scala/org/loopring/lightcone/ethereum/RingBatchGenerator.scala
+++ ethereum/src/main/scala/org/loopring/lightcone/ethereum/RingBatchGenerator.scala
@@ -16,6 +16,7 @@
 
 package org.loopring.lightcone.ethereum.data
 
+import org.web3j.crypto._
 import org.web3j.utils.Numeric
 import org.loopring.lightcone.proto.core._
 
@@ -27,8 +28,62 @@ trait RingBatchGenerator {
 class RingBatchGeneratorImpl(context: XRingBatchContext)
   extends RingBatchGenerator {
 
+  private def isNullOrEmpty(str: String) = {
+    str == null || str.length == 0
+  }
+
+  private def ringBatchHash(xRingBatch: XRingBatch) = {
+    val ringHashes = xRingBatch.rings.map(
+      xring ⇒ {
+        val bitstream = new Bitstream
+        val orders = xring.orderIndexes.map(i ⇒ xRingBatch.orders(i))
+        orders.foreach(o ⇒ {
+          bitstream.addHex(o.hash)
+          bitstream.addUint16(o.feeParams.get.waiveFeePercentage)
+        })
+        Numeric.toHexString(Hash.sha3(bitstream.getPackedBytes))
+      }
+    )
+
+    val ringHashesXor = ringHashes.tail.foldLeft(ringHashes.head) {
+      (h1: String, h2: String) ⇒
+        {
+          val bigInt1 = Numeric.decodeQuantity(h1)
+          val bigInt2 = Numeric.decodeQuantity(h2)
+          val bigIntXor = bigInt1.xor(bigInt2)
+          Numeric.encodeQuantity(bigIntXor)
+        }
+    }
+
+    var feeRecipient = xRingBatch.feeRecipient
+    if (feeRecipient == null || feeRecipient.length == 0) {
+      feeRecipient = xRingBatch.transactionOrigin
+    }
+    var miner = xRingBatch.miner
+    if (miner == null || miner.length == 0 || miner.equalsIgnoreCase(feeRecipient)) {
+      miner = "0x0"
+    }
+
+    val ringBatchBits = new Bitstream
+    ringBatchBits.addAddress(feeRecipient, true)
+    ringBatchBits.addAddress(miner, true)
+    ringBatchBits.addHex(ringHashesXor)
+
+    Numeric.toHexString(Hash.sha3(ringBatchBits.getPackedBytes))
+  }
+
   private def sign(xRingBatch: XRingBatch) = {
-    xRingBatch
+    val hash = ringBatchHash(xRingBatch)
+    val credentials = Credentials.create(context.minerPrivateKey)
+    val sigData = Sign.signMessage(
+      Numeric.hexStringToByteArray(hash),
+      credentials.getEcKeyPair
+    )
+
+    val sigBytes = sigData.getR ++ sigData.getS
+    val sig = Numeric.toHexString(sigBytes)
+
+    xRingBatch.copy(hash = hash, sig = sig)
   }
 
   def generateAndSignRingBatch(orders: Seq[Seq[XRawOrder]]): XRingBatch = {
@@ -49,11 +104,11 @@ class RingBatchGeneratorImpl(context: XRingBatchContext)
       .toMap
 
     val xrings = orders.map(orders ⇒ {
-      val orderIndexes = orders.map(order ⇒ ordersHashIndexMap(order.hash))
+      val orderIndexes = orders.map(o ⇒ ordersHashIndexMap(o.hash))
       new XRingBatch.XRing(orderIndexes)
     })
 
-    val xringBatch = new XRingBatch()
+    val xRingBatch = new XRingBatch()
       .withFeeRecipient(context.feeRecipient)
       .withMiner(context.miner)
       .withRings(xrings)
@@ -61,6 +116,6 @@ class RingBatchGeneratorImpl(context: XRingBatchContext)
       .withSignAlgorithm(XSigningAlgorithm.ALGO_ETHEREUM)
       .withTransactionOrigin(context.transactionOrigin)
 
-    sign(xringBatch)
+    sign(xRingBatch)
   }
 }
