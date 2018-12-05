ac85dee5b9e3ecb8219934ff2f729975ada2cfd1
kongliangzhong <kongliangzhong@gmail.com>
2018-12-05 20:58:30 +0800

    request and parse block data from eth node

--- indexer/src/main/scala/org/loopring/lightcone/indexer/Main.scala
+++ indexer/src/main/scala/org/loopring/lightcone/indexer/Main.scala
@@ -16,10 +16,85 @@
 
 package org.loopring.lightcone.indexer
 
+import scala.io.Source
+import scala.collection.JavaConverters._
+
+import java.io.PrintWriter
+import java.util.Timer
+import java.util.TimerTask
+
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterNumber
+import org.web3j.protocol.core.methods.response.EthBlock
+import org.web3j.protocol.core.methods.response.Transaction
+
 // The Ethereum indexer
 object Main {
+  val blockNumFile = ".block-num"
+  val infuraUrl = ""
+  val rinkebyUrl = "https://rinkeby.infura.io/hM4sFGiBdqbnGTxk5YT2"
 
   def main(args: Array[String]): Unit = {
+    val web3j = Web3j.build(new HttpService(rinkebyUrl))
+    var fromBlockNumber = getFromBlockNumber
+    var toBlockNumber = getLatestBlockNumber(web3j)
+    val txHandler = new TransactionHandlerImpl
+
+    val timer = new Timer
+    val task = new TimerTask {
+      def run = {
+        if (toBlockNumber > fromBlockNumber) {
+          processEthereumBlocks(web3j, fromBlockNumber, toBlockNumber, txHandler)
+          fromBlockNumber = toBlockNumber + 1
+        }
+
+        toBlockNumber = getLatestBlockNumber(web3j)
+      }
+    }
+
+    timer.schedule(task, 5000L, 1000L)
+  }
+
+  private def getFromBlockNumber: Long =
+    try {
+      val blockNumStr = Source.fromFile(blockNumFile).getLines.mkString.trim
+      blockNumStr.toLong
+    } catch {
+      case _: Throwable ⇒ 0L
+    }
+
+  private def getLatestBlockNumber(web3j: Web3j): Long = {
+    val ethBlockNumber = web3j.ethBlockNumber().send
+    ethBlockNumber.getBlockNumber.longValue
+  }
+
+  private def processEthereumBlocks(
+    web3j: Web3j,
+    fromBlock: Long,
+    toBlock: Long,
+    txHandler: TransactionHandler
+  ) {
+    (fromBlock to toBlock).foreach(blockNum ⇒ {
+      val blockParameter = new DefaultBlockParameterNumber(blockNum)
+      val txsOfBlock = web3j
+        .ethGetBlockByNumber(blockParameter, true)
+        .send
+        .getBlock
+        .getTransactions
+        .asScala
+
+      txsOfBlock.foreach(tx ⇒ txHandler.handle(tx.get.asInstanceOf[Transaction]))
+
+      new PrintWriter(blockNumFile) {
+        try {
+          write("" + blockNum)
+        } finally {
+          close
+        }
+      }
+
+    })
   }
 
 }
