28b6183d191f226bc7b4b6a34751e554c0739472
Daniel Wang <daniel@loopring.org>
2018-12-11 02:17:28 -0500

    more

--- actors/src/test/scala/org/loopring/lightcone/actors/core/CoreActorsIntegrationCommonSpec.scala
+++ actors/src/test/scala/org/loopring/lightcone/actors/core/CoreActorsIntegrationCommonSpec.scala
@@ -51,7 +51,7 @@ abstract class CoreActorsIntegrationCommonSpec(
     marketId: XMarketId,
     configStr: String
 )
-  extends TestKit(ActorSystem("test", ConfigFactory.load()))
+  extends TestKit(ActorSystem("lightcone_test", ConfigFactory.load()))
   with ImplicitSender
   with Matchers
   with WordSpecLike
@@ -64,7 +64,8 @@ abstract class CoreActorsIntegrationCommonSpec(
   import CoreActorsIntegrationCommonSpec._
 
   override def afterAll: Unit = {
-    TestKit.shutdownActorSystem(system)
+    super.afterAll()
+    TestKit.shutdownActorSystem(system, 10.seconds, false)
   }
 
   implicit val marketId_ = marketId
