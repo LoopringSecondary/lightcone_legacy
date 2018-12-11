2e4c5c9ae1aa27d4965577c81f51212f06c06588
lyd <lyddream@126.com>
2018-12-11 20:07:29 +0800

    add decode function result

--- ethereum/src/test/scala/org/loopring/lightcone/ethereum/abi/ERC20ABISpec.scala
+++ ethereum/src/test/scala/org/loopring/lightcone/ethereum/abi/ERC20ABISpec.scala
@@ -82,18 +82,34 @@ class ERC20ABISpec extends FlatSpec with Matchers with BeforeAndAfterAll {
     }
   }
 
-  "encodeBalanceOfFunction" should "encode class Parms of balaceOf function to  input" in {
+  "encodeBalanceOfFunction" should "encode class Parmas of balanceOf function to  input" in {
     val parms = BalanceOfFunction.Parms(_owner = "0x8fd3121013a07c57f0d69646e86e7a4880b467b7")
     val input = erc20abi.balanceOf.pack(parms)
     info(input)
     input should be("0x70a082310000000000000000000000008fd3121013a07c57f0d69646e86e7a4880b467b7")
   }
 
-  "encodeAllowanceFunction" should "encode class Parms of allowance function to  input" in {
+  "encodeAllowanceFunction" should "encode class Parmas of allowance function to  input" in {
     val parms = AllowanceFunction.Parms(_owner = "0x8fd3121013a07c57f0d69646e86e7a4880b467b7", _spender = "0xf105c622edc68b9e4e813e631cb534940f5cc509")
     val input = erc20abi.allowance.pack(parms)
     info(input)
     input should be("0xdd62ed3e0000000000000000000000008fd3121013a07c57f0d69646e86e7a4880b467b7000000000000000000000000f105c622edc68b9e4e813e631cb534940f5cc509")
+  }
+
+  "decodeBalanceOfFunctionResult" should "decode eth call result to BalanceOfFunction Result" in {
+    val resp = "0x0000000000000000000000000000000000000000004a817c7ffffffb57e83800"
+    val result = erc20abi.balanceOf.unpackResult(resp)
+    result.map { res ⇒
+      res.balance.toString() should be("90071992547409900000000000")
+    }
+  }
+
+  "decodeAllowanceFunctionResult" should "decode eth call result to AllowanceFunction Result" in {
+    val resp = "0x0000000000000000000000000000000000000000004a817c7ffffffb57e83800"
+    val result = erc20abi.allowance.unpackResult(resp)
+    result.map { res ⇒
+      res.allowance.toString() should be("90071992547409900000000000")
+    }
   }
 
   "decodeApproveEvent" should "decode event data and assemble to class Approve" in {
