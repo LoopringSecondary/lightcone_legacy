b8832aac132e06365e066b916c9d62d5783292e0
于红雨 <yuhongyu04@126.com>
2018-12-20 22:42:22 +0800

    mini change

--- actors/src/test/scala/org/loopring/lightcone/actors/core/CoreActorsIntegrationSpec_SubmitOneOrder.scala
+++ actors/src/test/scala/org/loopring/lightcone/actors/core/CoreActorsIntegrationSpec_SubmitOneOrder.scala
@@ -16,15 +16,11 @@
 
 package org.loopring.lightcone.actors.core
 
-import akka.actor.{Actor, ActorLogging, Props}
 import akka.pattern._
-import akka.util.Timeout
-import com.google.protobuf.ByteString
 import org.loopring.lightcone.actors.support._
-import org.loopring.lightcone.lib.MarketHashProvider
 import org.loopring.lightcone.proto._
 
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.Await
 
 class CoreActorsIntegrationSpec_SubmitOneOrder
     extends CommonSpec("""
@@ -39,36 +35,12 @@ class CoreActorsIntegrationSpec_SubmitOneOrder
     with MultiAccountManagerSupport
     with MarketManagerSupport
     with OrderbookManagerSupport
-    with EthereumQueryMockSupport {
+    with EthereumQueryMockSupport
+    with OrderGenerateSupport {
 
   "submiting one from OrderHandleActor" must {
     "get depth in OrderbookManagerActor" in {
-      val rawOrder = XRawOrder(
-        owner = "0xa",
-        hash = "0xa11",
-        version = 1,
-        tokenS = LRC_TOKEN.address,
-        tokenB = WETH_TOKEN.address,
-        amountS = ByteString.copyFrom("10".zeros(18).toString(), "UTF-8"),
-        amountB = ByteString.copyFrom("1".zeros(18).toString(), "UTF-8"),
-        validSince = 1000,
-        state = Some(
-          XRawOrder.State(
-            createdAt = timeProvider.getTimeMillis,
-            updatedAt = timeProvider.getTimeMillis,
-            status = XOrderStatus.STATUS_NEW
-          )
-        ),
-        feeParams = Some(
-          XRawOrder.FeeParams(
-            tokenFee = LRC_TOKEN.address,
-            amountFee = ByteString.copyFrom("3".zeros(18).toString(), "utf-8")
-          )
-        ),
-        params = Some(XRawOrder.Params(validUntil = 2000)),
-        marketHash =
-          MarketHashProvider.convert2Hex(LRC_TOKEN.address, WETH_TOKEN.address)
-      )
+      val rawOrder = createRawOrder()
 
       val submitF = actors.get(OrderHandlerActor.name) ? XSubmitOrderReq(
         Some(rawOrder)
