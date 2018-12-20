d361f0b8278c1e4d68260b76798b38c59728ab50
lyd <lyddream@126.com>
2018-12-20 18:03:29 +0800

    test

--- actors/src/test/scala/org/loopring/lightcone/actors/jsonrpc/BalanceSpec.scala
+++ actors/src/test/scala/org/loopring/lightcone/actors/jsonrpc/BalanceSpec.scala
@@ -21,14 +21,14 @@ import akka.http.scaladsl.model._
 import org.loopring.lightcone.actors.RpcBinding
 import org.loopring.lightcone.actors.entrypoint.EntryPointActor
 import org.loopring.lightcone.actors.support._
-import org.loopring.lightcone.proto.XGetBalanceReq
+import org.loopring.lightcone.proto._
 import org.json4s.jackson.Serialization
 import org.json4s.DefaultFormats
 import org.loopring.lightcone.actors.core.MultiAccountManagerActor
-import org.loopring.lightcone.actors.validator.{
-  MessageValidationActor,
-  MultiAccountManagerMessageValidator
-}
+import org.loopring.lightcone.actors.validator._
+import akka.pattern._
+import com.google.protobuf.ByteString
+import org.web3j.utils.Numeric
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -64,20 +64,80 @@ class BalanceSpec
   val rpcReq =
     Map("id" → 1, "method" → "get_balance_and_allowance", "jsonrpc" → "2.0")
 
-  val responseFuture: Future[HttpResponse] = Http().singleRequest(
-    HttpRequest(
-      method = HttpMethods.POST,
-      uri = s"http://localhost:8080$relayUri",
-      entity = HttpEntity(
-        ContentTypes.`application/json`,
-        Serialization.write(rpcReq.+("params" → getBalanceReq))
+  for {
+    ba1 ← Http()
+      .singleRequest(
+        HttpRequest(
+          method = HttpMethods.POST,
+          uri = s"http://localhost:8080$relayUri",
+          entity = HttpEntity(
+            ContentTypes.`application/json`,
+            Serialization.write(rpcReq.+("params" → getBalanceReq))
+          )
+        )
       )
-    )
-  )
+      .flatMap(res ⇒ res.entity.dataBytes.map(_.utf8String).runReduce(_ + _))
+      .map { Serialization.read[JsonRpcResponse] }
+//      .map { res ⇒
+//        val ba = res.result.get.extract[XGetBalanceAndAllowancesRes]
+    ////        ba.balanceAndAllowanceMap.map(
+    ////          item ⇒
+    ////            (
+    ////              BigInt(item._2.balance.toByteArray),
+    ////              BigInt(item._2.allowance.toByteArray),
+    ////              BigInt(item._2.availableBalance.toByteArray),
+    ////              BigInt(item._2.availableAllowance.toByteArray)
+    ////            )
+    ////        )
+    ////      }
+//    orderRes ← (actors.get(MultiAccountManagerMessageValidator.name) ? XSubmitSimpleOrderReq()
+//      .withOwner(owner)
+//      .withOrder(
+//        XOrder()
+//          .withTokenS(LRC_TOKEN.address)
+    ////          .withTokenB((WETH_TOKEN.address))
+    ////          .withAmountS(
+    ////            ByteString
+    ////              .copyFrom(Numeric.hexStringToByteArray("0x54607fc96a60000"))
+    ////          )
+    ////          .withAmountS(
+    ////            ByteString
+    ////              .copyFrom(Numeric.hexStringToByteArray("0x54607fc96a60000"))
+    ////          )
+//      )).mapTo[XSubmitOrderRes]
 
-  val res = Await.result(responseFuture, 2 seconds)
+    ba2 ← Http()
+      .singleRequest(
+        HttpRequest(
+          method = HttpMethods.POST,
+          uri = s"http://localhost:8080$relayUri",
+          entity = HttpEntity(
+            ContentTypes.`application/json`,
+            Serialization.write(rpcReq.+("params" → getBalanceReq))
+          )
+        )
+      )
+      .flatMap(res ⇒ res.entity.dataBytes.map(_.utf8String).runReduce(_ + _))
+      .map { Serialization.read[JsonRpcResponse] }
+//      .map { res ⇒
+//        val ba = res.extract[XGetBalanceAndAllowancesRes]
+//        ba.balanceAndAllowanceMap.map(
+//          item ⇒
+//            (
+//              BigInt(item._2.balance.toByteArray),
+//              BigInt(item._2.allowance.toByteArray),
+//              BigInt(item._2.availableBalance.toByteArray),
+//              BigInt(item._2.availableAllowance.toByteArray)
+//            )
+//        )
+//      }
 
-  println(res)
+  } yield {
+
+    println(s"ba1：$ba1")
+    println(s"ba2: $ba2")
+//    println(orderRes)
+  }
 
   Thread.sleep(60 * 60 * 1000)
 }
