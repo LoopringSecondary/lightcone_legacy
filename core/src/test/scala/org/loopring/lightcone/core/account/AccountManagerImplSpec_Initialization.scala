b45dbc350d68e16a303c00facceec3f5c1b05e72
Daniel Wang <daniel@loopring.org>
2018-12-23 09:52:14 -0500

    more

--- core/src/test/scala/org/loopring/lightcone/core/account/AccountManagerImplSpec_Initialization.scala
+++ core/src/test/scala/org/loopring/lightcone/core/account/AccountManagerImplSpec_Initialization.scala
@@ -14,140 +14,125 @@
  * limitations under the License.
  */
 
-// /*
-//  * Copyright 2018 Loopring Foundation
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+package org.loopring.lightcone.core.account
 
-// package org.loopring.lightcone.core.account
+import org.loopring.lightcone.core.OrderAwareSpec
+import org.loopring.lightcone.core.data._
+import org.loopring.lightcone.core._
+import org.loopring.lightcone.proto._
+import org.scalatest._
 
-// import org.loopring.lightcone.core.OrderAwareSpec
-// import org.loopring.lightcone.core.data._
-// import org.loopring.lightcone.proto._
-// import org.scalatest._
+class AccountManagerImplSpec_Initialization extends OrderAwareSpec {
+  "reinitialization of tokenS balance to smaller values" should "cancel existing orders" in {
+    dai.setBalanceAndAllowance(1000 !, 10000 !)
 
-// class AccountManagerImplSpec_Initialization extends OrderAwareSpec {
-//   "reinitialization of tokenS balance to smaller values" should "cancel existing orders" in {
-//     dai.setBalanceAndAllowance(1000, 10000)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 0 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    dai.setBalanceAndAllowance(499 !, 10000 !)
 
-//     resetUpdatedOrders()
-//     dai.setBalanceAndAllowance(499, 10000)
+    updatedOrders.size should be(6)
 
-//     updatedOrders.size should be(6)
+    updatedOrders foreach {
+      case (id, order) =>
+        order.status should be(XOrderStatus.STATUS_CANCELLED_LOW_BALANCE)
+    }
+  }
 
-//     updatedOrders foreach {
-//       case (id, order) =>
-//         order.status should be(XOrderStatus.STATUS_CANCELLED_LOW_BALANCE)
-//     }
-//   }
+  "reinitialization of tokenFee balance to smaller values" should "cancel existing orders" in {
+    dai.setBalanceAndAllowance(1000 !, 10000 !)
+    lrc.setBalanceAndAllowance(100 !, 1000 !)
 
-//   "reinitialization of tokenFee balance to smaller values" should "cancel existing orders" in {
-//     dai.setBalanceAndAllowance(1000, 10000)
-//     lrc.setBalanceAndAllowance(100, 1000)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 10 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1, 10))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    lrc.setBalanceAndAllowance(49 !, 10000 !)
 
-//     resetUpdatedOrders()
-//     lrc.setBalanceAndAllowance(49, 10000)
+    updatedOrders.size should be(6)
 
-//     updatedOrders.size should be(6)
+    updatedOrders foreach {
+      case (id, order) =>
+        order.status should be(XOrderStatus.STATUS_CANCELLED_LOW_FEE_BALANCE)
+    }
+  }
 
-//     updatedOrders foreach {
-//       case (id, order) =>
-//         order.status should be(XOrderStatus.STATUS_CANCELLED_LOW_FEE_BALANCE)
-//     }
-//   }
+  "reinitialization of tokenS allowance to smaller values" should "scale down existing orders" in {
+    dai.setBalanceAndAllowance(1000 !, 1000 !)
 
-//   "reinitialization of tokenS allowance to smaller values" should "scale down existing orders" in {
-//     dai.setBalanceAndAllowance(1000, 1000)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 0 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    dai.setBalanceAndAllowance(1000 !, 500 !)
 
-//     resetUpdatedOrders()
-//     dai.setBalanceAndAllowance(1000, 500)
+    updatedOrders.size should be(5)
 
-//     updatedOrders.size should be(5)
+    updatedOrders foreach {
+      case (id, order) =>
+        order.status should be(XOrderStatus.STATUS_PENDING)
+        order.reserved should be(orderState(0 !, 0 !, 0 !))
+        order.actual should be(orderState(0 !, 0 !, 0 !))
+    }
+  }
 
-//     updatedOrders foreach {
-//       case (id, order) =>
-//         order.status should be(XOrderStatus.STATUS_PENDING)
-//         order.reserved should be(orderState(0, 0, 0))
-//         order.actual should be(orderState(0, 0, 0))
-//     }
-//   }
+  "reinitialization of tokenFee allowance to smaller values" should "scale down existing orders" in {
+    dai.setBalanceAndAllowance(1000 !, 1000 !)
+    lrc.setBalanceAndAllowance(100 !, 100 !)
 
-//   "reinitialization of tokenFee allowance to smaller values" should "scale down existing orders" in {
-//     dai.setBalanceAndAllowance(1000, 1000)
-//     lrc.setBalanceAndAllowance(100, 100)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 10 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1, 10))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    lrc.setBalanceAndAllowance(100 !, 50 !)
 
-//     resetUpdatedOrders()
-//     lrc.setBalanceAndAllowance(100, 50)
+    updatedOrders.size should be(5)
 
-//     updatedOrders.size should be(5)
+    updatedOrders foreach {
+      case (id, order) =>
+        order.status should be(XOrderStatus.STATUS_PENDING)
+        order.reserved should be(orderState(100 !, 0 !, 0 !))
+        order.actual should be(orderState(0 !, 0 !, 0 !))
+    }
+  }
 
-//     updatedOrders foreach {
-//       case (id, order) =>
-//         order.status should be(XOrderStatus.STATUS_PENDING)
-//         order.reserved should be(orderState(100, 0, 0))
-//         order.actual should be(orderState(0, 0, 0))
-//     }
-//   }
+  "reinitialization of tokenS balance to 0" should "cancel all orders" in {
+    dai.setBalanceAndAllowance(1000 !, 10000 !)
 
-//   "reinitialization of tokenS balance to 0" should "cancel all orders" in {
-//     dai.setBalanceAndAllowance(1000, 10000)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 0 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    dai.setBalanceAndAllowance(0 !, 10000 !)
 
-//     resetUpdatedOrders()
-//     dai.setBalanceAndAllowance(0, 10000)
+    updatedOrders.size should be(10)
+    orderPool.size should be(0)
+  }
 
-//     updatedOrders.size should be(10)
-//     orderPool.size should be(0)
-//   }
+  "reinitialization of tokenFee balance 0" should "cancel all orders" in {
+    dai.setBalanceAndAllowance(1000 !, 10000 !)
+    lrc.setBalanceAndAllowance(100 !, 1000 !)
 
-//   "reinitialization of tokenFee balance 0" should "cancel all orders" in {
-//     dai.setBalanceAndAllowance(1000, 10000)
-//     lrc.setBalanceAndAllowance(100, 1000)
+    (1 to 10) foreach { i =>
+      submitOrder(sellDAI(100 !, 1 !, 10 !))
+    }
+    orderPool.size should be(10)
 
-//     (1 to 10) foreach { i =>
-//       submitOrder(sellDAI(100, 1, 10))
-//     }
-//     orderPool.size should be(10)
+    resetUpdatedOrders()
+    lrc.setBalanceAndAllowance(0 !, 10000 !)
 
-//     resetUpdatedOrders()
-//     lrc.setBalanceAndAllowance(0, 10000)
-
-//     updatedOrders.size should be(10)
-//     orderPool.size should be(0)
-//   }
-// }
+    updatedOrders.size should be(10)
+    orderPool.size should be(0)
+  }
+}
