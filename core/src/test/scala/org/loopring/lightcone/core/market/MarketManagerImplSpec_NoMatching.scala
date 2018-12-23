7ef9e059b131cc490f4a15fb7b34316c7c08aed2
Daniel Wang <daniel@loopring.org>
2018-12-23 00:55:35 -0500

    more

--- core/src/test/scala/org/loopring/lightcone/core/market/MarketManagerImplSpec_NoMatching.scala
+++ core/src/test/scala/org/loopring/lightcone/core/market/MarketManagerImplSpec_NoMatching.scala
@@ -14,120 +14,100 @@
  * limitations under the License.
  */
 
-// /*
-//  * Copyright 2018 Loopring Foundation
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+package org.loopring.lightcone.core.market
 
-// package org.loopring.lightcone.core.market
+import org.loopring.lightcone.core.base._
+import org.loopring.lightcone.core.data._
+import org.loopring.lightcone.proto._
+import org.loopring.lightcone.core._
+import XOrderStatus._
 
-// import org.loopring.lightcone.core.base._
-// import org.loopring.lightcone.core.data._
-// import org.loopring.lightcone.proto._
-// import org.loopring.lightcone.core._
-// import XOrderStatus._
+class MarketManagerImplSpec_NoMatching extends MarketAwareSpec {
 
-// class MarketManagerImplSpec_NoMatching extends MarketAwareSpec {
+  "MarketManager" should "reject orders whose original size is dust" in {
+    var order = sellGTO(1000, 1)
+    (fakeDustOrderEvaluator.isOriginalDust _).when(order).returns(true)
 
-//   "MarketManager" should "reject orders whose original size is dust" in {
-//     var order = sellGTO(1000, 1)
-//     (fakeDustOrderEvaluator.isOriginalDust _).when(order).returns(true)
+    val result = marketManager.submitOrder(order, 0)
+    result should be(emptyMatchingResult(order, STATUS_DUST_ORDER))
 
-//     val result = marketManager.submitOrder(order, 0)
-//     result should be(emptyMatchingResult(order, STATUS_DUST_ORDER))
+    noMatchingActivity()
+  }
 
-//     noMatchingActivity()
-//   }
+  "MarketManager" should "reject orders whose actual size is dust" in {
+    var order = sellGTO(1000, 1)
+    (fakeDustOrderEvaluator.isOriginalDust _).when(order).returns(false)
+    (fakeDustOrderEvaluator.isActualDust _).when(order).returns(true)
 
-//   "MarketManager" should "reject orders whose actual size is dust" in {
-//     var order = sellGTO(1000, 1)
-//     (fakeDustOrderEvaluator.isOriginalDust _).when(order).returns(false)
-//     (fakeDustOrderEvaluator.isActualDust _).when(order).returns(true)
+    val result = marketManager.submitOrder(order, 0)
+    result should be(emptyMatchingResult(order, STATUS_COMPLETELY_FILLED))
 
-//     val result = marketManager.submitOrder(order, 0)
-//     result should be(emptyMatchingResult(order, STATUS_COMPLETELY_FILLED))
+    noMatchingActivity()
+  }
 
-//     noMatchingActivity()
-//   }
+  "MarketManager" should "accept sell orders" in {
+    var order1 = actualNotDust(sellGTO(100000, 100))
+    var order2 = actualNotDust(sellGTO(100000, 101))
 
-//   "MarketManager" should "accept sell orders" in {
-//     var order1 = actualNotDust(sellGTO(100000, 100))
-//     var order2 = actualNotDust(sellGTO(100000, 101))
+    (fakePendingRingPool.getOrderPendingAmountS _).when(*).returns(0)
+    (fakeAggregator.getOrderbookUpdate _).when(0).returns(XOrderbookUpdate())
 
-//     (fakePendingRingPool.getOrderPendingAmountS _).when(*).returns(0)
-//     (fakeAggregator.getOrderbookUpdate _).when(0).returns(XOrderbookUpdate())
+    var result = marketManager.submitOrder(order1, 0)
+    result should be(emptyMatchingResult(order1, STATUS_PENDING))
 
-//     var result = marketManager.submitOrder(order1, 0)
-//     result should be(emptyMatchingResult(order1, STATUS_PENDING))
+    result = marketManager.submitOrder(order2, 0)
+    result should be(emptyMatchingResult(order2, STATUS_PENDING))
 
-//     result = marketManager.submitOrder(order2, 0)
-//     result should be(emptyMatchingResult(order2, STATUS_PENDING))
+    noMatchingActivity()
 
-//     noMatchingActivity()
+    marketManager.getNumOfSellOrders() should be(2)
+    marketManager.getNumOfBuyOrders() should be(0)
+    marketManager.getNumOfOrders() should be(2)
 
-//     marketManager.getNumOfSellOrders() should be(2)
-//     marketManager.getNumOfBuyOrders() should be(0)
-//     marketManager.getNumOfOrders() should be(2)
+    marketManager.getSellOrders(3) should be(
+      Seq(
+        order1.copy(status = STATUS_PENDING),
+        order2.copy(status = STATUS_PENDING)
+      )
+    )
 
-//     marketManager.getSellOrders(3) should be(
-//       Seq(
-//         order1.copy(status = STATUS_PENDING),
-//         order2.copy(status = STATUS_PENDING)
-//       )
-//     )
+    marketManager.getSellOrders(1) should be(
+      Seq(order1.copy(status = STATUS_PENDING))
+    )
 
-//     marketManager.getSellOrders(1) should be(
-//       Seq(
-//         order1.copy(status = STATUS_PENDING)
-//       )
-//     )
+    marketManager.getBuyOrders(100) should be(Nil)
+  }
 
-//     marketManager.getBuyOrders(100) should be(Nil)
-//   }
+  "MarketManager" should "accept buy orders" in {
+    var order1 = actualNotDust(buyGTO(100, 100000))
+    var order2 = actualNotDust(buyGTO(101, 100000))
 
-//   "MarketManager" should "accept buy orders" in {
-//     var order1 = actualNotDust(buyGTO(100, 100000))
-//     var order2 = actualNotDust(buyGTO(101, 100000))
+    (fakePendingRingPool.getOrderPendingAmountS _).when(*).returns(0)
+    (fakeAggregator.getOrderbookUpdate _).when(0).returns(XOrderbookUpdate())
 
-//     (fakePendingRingPool.getOrderPendingAmountS _).when(*).returns(0)
-//     (fakeAggregator.getOrderbookUpdate _).when(0).returns(XOrderbookUpdate())
+    var result = marketManager.submitOrder(order1, 0)
+    result should be(emptyMatchingResult(order1, STATUS_PENDING))
 
-//     var result = marketManager.submitOrder(order1, 0)
-//     result should be(emptyMatchingResult(order1, STATUS_PENDING))
+    result = marketManager.submitOrder(order2, 0)
+    result should be(emptyMatchingResult(order2, STATUS_PENDING))
 
-//     result = marketManager.submitOrder(order2, 0)
-//     result should be(emptyMatchingResult(order2, STATUS_PENDING))
+    noMatchingActivity()
 
-//     noMatchingActivity()
+    marketManager.getNumOfSellOrders() should be(0)
+    marketManager.getNumOfBuyOrders() should be(2)
+    marketManager.getNumOfOrders() should be(2)
 
-//     marketManager.getNumOfSellOrders() should be(0)
-//     marketManager.getNumOfBuyOrders() should be(2)
-//     marketManager.getNumOfOrders() should be(2)
+    marketManager.getBuyOrders(3) should be(
+      Seq(
+        order1.copy(status = STATUS_PENDING),
+        order2.copy(status = STATUS_PENDING)
+      )
+    )
 
-//     marketManager.getBuyOrders(3) should be(
-//       Seq(
-//         order2.copy(status = STATUS_PENDING),
-//         order1.copy(status = STATUS_PENDING)
-//       )
-//     )
+    marketManager.getBuyOrders(1) should be(
+      Seq(order1.copy(status = STATUS_PENDING))
+    )
 
-//     marketManager.getBuyOrders(1) should be(
-//       Seq(
-//         order2.copy(status = STATUS_PENDING)
-//       )
-//     )
-
-//     marketManager.getSellOrders(100) should be(Nil)
-//   }
-// }
+    marketManager.getSellOrders(100) should be(Nil)
+  }
+}
