e7faedafcfa86f799da74edb19e4a9e3436a09cc
Daniel Wang <dong77@gmail.com>
2018-12-24 22:32:14 -0500

    Update OrderServiceSpec.scala

--- persistence/src/test/scala/org/loopring/lightcone/persistence/services/OrderServiceSpec.scala
+++ persistence/src/test/scala/org/loopring/lightcone/persistence/services/OrderServiceSpec.scala
@@ -469,9 +469,7 @@ class OrderServiceSpec extends ServiceSpec[OrderService] {
       result.mapTo[(Seq[XUserCancelOrderResult.Result], Int, Int)],
       5.second
     )
-    res._1.map { o =>
-      assert(o.order.isDefined)
-    }
+    res._1.map { o => assert(o.order.isDefined) }
     val x = res._1.length === 3 && !res._1.exists(
       _.error !== XErrorCode.ERR_NONE
     ) && res._2 === 6 && res._3 === 3
