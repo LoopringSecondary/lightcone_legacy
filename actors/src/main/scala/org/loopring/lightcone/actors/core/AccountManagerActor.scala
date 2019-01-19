4c188f010453c04de7171bdb2d262f09d07b30e9
lyd <lyddream@126.com>
2019-01-19 22:22:34 +0800

    delete log

--- actors/src/main/scala/org/loopring/lightcone/actors/core/AccountManagerActor.scala
+++ actors/src/main/scala/org/loopring/lightcone/actors/core/AccountManagerActor.scala
@@ -226,7 +226,6 @@ class AccountManagerActor(
 
     case req: OrderFilledEvent
         if req.header.nonEmpty && req.getHeader.txStatus == TxStatus.TX_STATUS_SUCCESS =>
-      println(s"received OrderFilledEvent ${req}")
       //收到filledEvent后，submitOrAdjustOrder会调用adjust方法
       for {
         orderOpt <- dbModule.orderService.getOrder(req.orderHash)
