c7ce782ffd69df051d17c87cb81316904f3e04b0
lyd <lyddream@126.com>
2019-01-21 11:56:32 +0800

    extractor

--- actors/src/main/scala/org/loopring/lightcone/actors/ethereum/event/OHLCRawDataExtractor.scala
+++ actors/src/main/scala/org/loopring/lightcone/actors/ethereum/event/OHLCRawDataExtractor.scala
@@ -1,5 +1,19 @@
 package org.loopring.lightcone.actors.ethereum.event
 
-class OHLCRawDataExtractor {
+import com.google.inject.Inject
+import org.loopring.lightcone.proto.{OrderFilledEvent, RawBlockData}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class OHLCRawDataExtractor @Inject()(
+                                      implicit
+                                      extractor: OrderFillEventExtractor,
+                                      val ec: ExecutionContext)
+  extends EventExtractor[OrderFilledEvent]{
+
+   def extract(block: RawBlockData): Future[Seq[OrderFilledEvent]] = {
+
+     extractor.extract(block)
+   }
 
 }
