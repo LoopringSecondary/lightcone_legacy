e5d8111a09a4a56201c22851c973ebebe3c11d4d
Daniel Wang <dong77@gmail.com>
2019-02-07 18:35:11 -0500

    add integration test framework (draft) (#419)

---
+++ relayer/src/test/scala/io/lightcone/relayer/integration/ExampleIntegrationSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.lightcone.relayer
+
+import io.lightcone.relayer.support._
+import io.lightcone.relayer.data._
+import io.lightcone.relayer.base._
+import io.lightcone.relayer.actors._
+import io.lightcone.core._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+import java.io.File
+
+import akka.actor._
+import com.google.inject.Guice
+import com.google.inject.Injector
+import com.typesafe.config.ConfigFactory
+import kamon.Kamon
+import net.codingwell.scalaguice.InjectorExtensions._
+import org.slf4s.Logging
+import scala.io.StdIn
+import scala.util.Try
+
+import org.scalatest._
+import org.slf4s.Logging
+
+// Please make sure in `mysql.conf` all database dals use the same database configuration.
+class ExampleIntegrationSpec extends IntegrationTesting {
+
+  "foo" must "bar" in {
+    testRpc {
+      val order: RawOrder = Addr(0) >> 12.1.lrc -> 23.0.weth -- 10.0.lrc
+      SubmitOrder.Req(Some(order))
+    } {
+      SubmitOrder.Res()
+    }
+  }
+
+}
---
+++ relayer/src/test/scala/io/lightcone/relayer/integration/TestHelpers.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.lightcone.relayer
+
+import io.lightcone.core._
+import akka.actor.ActorRef
+import akka.pattern.ask
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import org.scalatest._
+
+trait TestHelpers extends IntegrationConstants with Matchers {
+  implicit val timeout = akka.util.Timeout(10 seconds)
+  def entrypoint: ActorRef
+
+  case class _SomeToken(
+      amount: Double,
+      tokenAddress: String) {
+    def ->(another: _SomeToken) = _OrderRep(this, another, None)
+  }
+
+  case class _OrderRep(
+      sell: _SomeToken,
+      buy: _SomeToken,
+      fee: Option[_SomeToken]) {
+    def --(fee: _SomeToken) = copy(fee = Some(fee))
+  }
+
+  implicit class _RichDoubleAmount(v: Double) {
+    def ^(str: String) = _SomeToken(v, str)
+    def lrc = _SomeToken(v, LRC)
+    def gto = _SomeToken(v, GTO)
+    def weth = _SomeToken(v, WETH)
+  }
+
+  implicit class _RichStringAddress(owner: String) {
+
+    def >>(or: _OrderRep): RawOrder =
+      RawOrder(
+        owner = owner,
+        tokenS = or.sell.tokenAddress,
+        tokenB = or.buy.tokenAddress
+      )
+  }
+
+  implicit class _RichActorRef(actor: ActorRef) {
+
+    def ??[T](msg: Any): T = {
+      Await.result(actor ? msg, timeout.duration).asInstanceOf[T]
+    }
+  }
+
+  def send(req: Any) = Request(req)
+
+  def testRpc(req: => Any)(res: => Any) = send(req).expects(res)
+
+  case class Request(req: Any) {
+
+    def expects(res: Any) = {
+      Await.result(entrypoint ? req, timeout.duration) should be(res)
+    }
+  }
+
+  // TODO
+  def killActors(name: String) = {}
+
+}
