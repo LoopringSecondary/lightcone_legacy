3171f3738bdd1a73e02a6ba2ff57db9e6368d1ab
Daniel Wang <daniel@loopring.org>
2019-02-09 18:37:54 -0500

    x

--- relayer/src/main/scala/io/lightcone/relayer/actors/AccountManagerActor2.scala
+++ relayer/src/main/scala/io/lightcone/relayer/actors/AccountManagerActor2.scala
@@ -21,6 +21,7 @@ import akka.actor._
 import akka.event.LoggingReceive
 import akka.pattern._
 import akka.util.Timeout
+import com.google.protobuf.ByteString
 import com.typesafe.config.Config
 import io.lightcone.relayer.base._
 import io.lightcone.relayer.data._
@@ -33,8 +34,7 @@ import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
 // Owner: Hongyu
-//TODO:如果刷新时间太长，或者读取次数超过一个值，就重新从以太坊读取balance/allowance，并reset这个时间和读取次数。
-
+// TODO:如果刷新时间太长，或者读取次数超过一个值，就重新从以太坊读取balance/allowance，并reset这个时间和读取次数。
 class AccountManagerActor2(
     val owner: String
   )(
@@ -54,6 +54,8 @@ class AccountManagerActor2(
     with ActorLogging {
 
   import ErrorCode._
+  import OrderStatus._
+  import TxStatus._
 
   implicit val processor: UpdatedOrdersProcessor = this
 
@@ -120,10 +122,10 @@ class AccountManagerActor2(
     case req @ Notify(KeepAliveActor.NOTIFY_MSG, _) =>
       sender ! req
 
-    // case ActorRecover.RecoverOrderReq(Some(xraworder)) =>
-    //   submitOrder(xraworder).map { _ =>
-    //     ActorRecover.OrderRecoverResult(xraworder.hash, true)
-    //   }.sendTo(sender)
+    case ActorRecover.RecoverOrderReq(Some(xraworder)) =>
+      resubmitOrder(xraworder).map { _ =>
+        ActorRecover.OrderRecoverResult(xraworder.hash, true)
+      }.sendTo(sender)
 
     case GetBalanceAndAllowances.Req(addr, tokens, _) =>
       (for {
@@ -143,24 +145,27 @@ class AccountManagerActor2(
         result = GetBalanceAndAllowances.Res(addr, balanceAndAllowanceMap)
       } yield result).sendTo(sender)
 
-    // case req @ SubmitOrder.Req(Some(raworder)) =>
-    //   (for {
-    //     //check通过再保存到数据库，以及后续处理
-    //     _ <- Future { accountCutoffState.checkOrderCutoff(raworder) }
-    //     _ <- checkOrderCanceled(raworder) //取消订单，单独查询以太坊
-    //     newRaworder = if (raworder.validSince > timeProvider.getTimeSeconds()) {
-    //       raworder.withStatus(STATUS_PENDING_ACTIVE)
-    //     } else raworder
+    case req @ SubmitOrder.Req(Some(raworder)) =>
+      (for {
+        //check通过再保存到数据库，以及后续处理
+        _ <- Future { accountCutoffState.checkOrderCutoff(raworder) }
+        _ <- checkOrderCanceled(raworder) //取消订单，单独查询以太坊
+        newRaworder = if (raworder.validSince > timeProvider.getTimeSeconds()) {
+          raworder.withStatus(STATUS_PENDING_ACTIVE)
+        } else raworder
 
-    //     resRawOrder <- (orderPersistenceActor ? req
-    //       .copy(rawOrder = Some(newRaworder)))
-    //       .mapAs[RawOrder]
-    //     resOrder <- (resRawOrder.getState.status match {
-    //       case STATUS_PENDING_ACTIVE =>
-    //         Future.successful(resRawOrder.toOrder)
-    //       case _ => submitOrder(resRawOrder)
-    //     }).mapAs[Order]
-    // //   } yield SubmitOrder.Res(Some(resOrder))) sendTo sender
+        resRawOrder <- (orderPersistenceActor ? req
+          .copy(rawOrder = Some(newRaworder)))
+          .mapAs[RawOrder]
+
+        resOrder <- (resRawOrder.getState.status match {
+          case STATUS_PENDING_ACTIVE =>
+            Future.successful(resRawOrder.toOrder)
+          case _ => resubmitOrder(resRawOrder)
+        }).mapAs[Order]
+
+        result = SubmitOrder.Res(Some(resOrder))
+      } yield result) sendTo sender
 
     case req @ CancelOrder.Req("", addr, _, None, _) => //按照Owner取消订单
       (for {
@@ -200,7 +205,7 @@ class AccountManagerActor2(
         }
       } yield result) sendTo sender
 
-    // //为了减少以太坊的查询量，需要每个block汇总后再批量查询，因此不使用TransferEvent
+    // 为了减少以太坊的查询量，需要每个block汇总后再批量查询，因此不使用TransferEvent
     case req: AddressBalanceUpdated =>
       assert(req.address == owner)
       manager
@@ -208,125 +213,83 @@ class AccountManagerActor2(
 
     case req: AddressAllowanceUpdated =>
       assert(req.address == owner)
-      manager
-        .setAllowance(req.token, BigInt(req.allowance.toByteArray))
+      manager.setAllowance(req.token, BigInt(req.allowance.toByteArray))
 
-    // //ownerCutoff
+    // ownerCutoff
     case req @ CutoffEvent(Some(header), broker, owner, "", cutoff) //
-        if broker == owner && header.txStatus == TxStatus.TX_STATUS_SUCCESS =>
+        if broker == owner && header.txStatus == TX_STATUS_SUCCESS =>
       accountCutoffState.setCutoff(cutoff)
       manager.handleCutoff(cutoff)
 
-    // //ownerTokenPairCutoff  tokenPair ！= ""
+    // ownerTokenPairCutoff  tokenPair ！= ""
     case req @ CutoffEvent(Some(header), broker, owner, marketHash, cutoff) //
-        if broker == owner && header.txStatus == TxStatus.TX_STATUS_SUCCESS =>
+        if broker == owner && header.txStatus == TX_STATUS_SUCCESS =>
       accountCutoffState.setTradingPairCutoff(marketHash, req.cutoff)
       manager.handleCutoff(cutoff, marketHash)
 
     // Currently we do not support broker-level cutoff
     case req @ CutoffEvent(Some(header), broker, owner, _, cutoff) //
-        if broker != owner && header.txStatus == TxStatus.TX_STATUS_SUCCESS =>
-      log.debug(s"received BrokerCutoffEvent $req")
+        if broker != owner && header.txStatus == TX_STATUS_SUCCESS =>
+      log.warning(s"not support this event yet: $req")
 
     case req: OrderFilledEvent //
-        if req.header.nonEmpty && req.getHeader.txStatus == TxStatus.TX_STATUS_SUCCESS =>
+        if req.header.nonEmpty && req.getHeader.txStatus == TX_STATUS_SUCCESS =>
       for {
         orderOpt <- dbModule.orderService.getOrder(req.orderHash)
-        _ <- swap(orderOpt.map(submitOrder))
+        _ <- swap(orderOpt.map(resubmitOrder))
       } yield Unit
   }
 
-  private def submitOrder(rawOrder: RawOrder): Future[Order] = {
-    null
-    //   val order = rawOrder.toOrder
-    //   val matchable: Matchable = order
-    //   log.debug(s"### submitOrder ${order}")
-    //   for {
-    //     _ <- if (matchable.amountFee > 0 && matchable.tokenS != matchable.tokenFee)
-    //       getReserveManagers(Seq(matchable.tokenS, matchable.tokenFee))
-    //     else
-    //       getReserveManagers(Seq(matchable.tokenS))
+  private def resubmitOrder(rawOrder: RawOrder): Future[Order] = {
+    val order = rawOrder.toOrder
+    val orderId = order.id
+    val matchable: Matchable = order
+    log.debug(s"### submitOrder ${order}")
+    for {
+      getFilledAmountRes <- (ethereumQueryActor ? GetFilledAmount.Req(
+        Seq(orderId)
+      )).mapAs[GetFilledAmount.Res]
 
-    //     getFilledAmountRes <- (ethereumQueryActor ? GetFilledAmount.Req(
-    //       Seq(matchable.id))).mapAs[GetFilledAmount.Res]
+      filledAmountS = getFilledAmountRes.filledAmountSMap
+        .getOrElse(orderId, ByteString.copyFrom("0".getBytes))
 
-    //     filledAmountS = getFilledAmountRes.filledAmountSMap
-    //       .getOrElse(matchable.id, ByteString.copyFrom("0".getBytes))
+      adjusted = matchable.withFilledAmountS(filledAmountS)
 
-    //     _matchable = matchable.withFilledAmountS(filledAmountS)
+      (successful, updatedOrders) <- manager.resubmitOrder(adjusted)
 
-    //     (successful, updatedOrders) = manager.synchronized {
-    //       val res = if (orderPool.contains(order.id)) {
-    //         manager.adjustOrder(_matchable.id, _matchable.outstanding.amountS)
-    //       } else {
-    //         manager.submitOrder(_matchable)
-    //       }
-    //       (res, orderPool.takeUpdatedOrders)
-    //     }
+      updatedOrder = updatedOrders.getOrElse(orderId, adjusted)
 
-    //     _ = if (!successful) {
-    //       val error = updatedOrders(matchable.id).status match {
-    //         case STATUS_INVALID_DATA => ERR_INVALID_ORDER_DATA
-    //         case STATUS_UNSUPPORTED_MARKET => ERR_INVALID_MARKET
-    //         case STATUS_SOFT_CANCELLED_TOO_MANY_ORDERS => ERR_TOO_MANY_ORDERS
-    //         case STATUS_SOFT_CANCELLED_DUPLICIATE => ERR_ORDER_ALREADY_EXIST
-    //         case other =>
-    //           log.error(s"unexpected failure order status $other")
-    //           ERR_INTERNAL_UNKNOWN
-    //       }
+      status = updatedOrder.status
+      _ = if (!successful) {
+        val error = status match {
+          case STATUS_SOFT_CANCELLED_LOW_BALANCE => ERR_LOW_BALANCE
+          case _ =>
+            log.error(s"unexpected failure order status $status")
+            ERR_INTERNAL_UNKNOWN
+        }
+        throw ErrorException(error, s"failed to submit order ${status}.")
+      }
 
-    //       throw ErrorException(
-    //         error,
-    //         s"failed to submit order with status:${matchable.status} in AccountManagerActor.")
-    //     }
+      result: Order = updatedOrder.copy(_reserved = None, _outstanding = None)
 
-    //     _ = log.debug(
-    //       s"updated matchable ${_matchable}\nfound ${updatedOrders.size} updated orders")
-
-    //     res <- processOrders(updatedOrders)
-
-    //     matchable_ = updatedOrders.getOrElse(matchable.id, _matchable)
-    //     order_ : Order = matchable_.copy(_reserved = None, _outstanding = None)
-    //   } yield order_
+      _ = log.debug(
+        s"submit order result:  ${result}",
+        s"with ${updatedOrders.size} updated orders"
+      )
+    } yield result
   }
 
-  // // TODO(hongyu): this is a bug here - if a order reservers two tokens, A, and B, if A's allowance
-  // // becomes very small, then we also need to release all reserved token B for this order.
-  // private def updateBalanceOrAllowance(
-  //   token: String)(retrieveUpdatedOrders: => Map[String, Matchable]) =
-  //   for {
-  //     _ <- getReserveManagers(Seq(token))
-  //     updatedOrders = retrieveUpdatedOrders
-  //     _ <- Future.sequence {
-  //       updatedOrders.values.map { order =>
-  //         order.status match {
-  //           case STATUS_SOFT_CANCELLED_LOW_BALANCE |
-  //             STATUS_SOFT_CANCELLED_LOW_FEE_BALANCE | //
-  //             STATUS_PENDING | //
-  //             STATUS_COMPLETELY_FILLED | //
-  //             STATUS_PARTIALLY_FILLED =>
-  //             Future.unit
-
-  //           case status =>
-  //             val msg =
-  //               s"unexpected order status caused by balance/allowance upate: $status"
-  //             log.error(msg)
-  //             throw ErrorException(ERR_INTERNAL_UNKNOWN, msg)
-  //         }
-  //       }
-  //     }
-  //     _ <- processOrders(updatedOrders)
-  //   } yield Unit
-
-  // def checkOrderCanceled(rawOrder: RawOrder) =
-  //   for {
-  //     res <- (ethereumQueryActor ? GetOrderCancellation.Req(
-  //       broker = address,
-  //       orderHash = rawOrder.hash)).mapAs[GetOrderCancellation.Res]
-  //   } yield if (res.cancelled)
-  //     throw ErrorException(
-  //       ERR_ORDER_VALIDATION_INVALID_CANCELED,
-  //       s"this order has been canceled.")
+  private def checkOrderCanceled(rawOrder: RawOrder) =
+    for {
+      res <- (ethereumQueryActor ? GetOrderCancellation.Req(
+        orderHash = rawOrder.hash
+      )).mapAs[GetOrderCancellation.Res]
+      _ = if (res.cancelled)
+        throw ErrorException(
+          ERR_ORDER_VALIDATION_INVALID_CANCELED,
+          s"this order has been canceled."
+        )
+    } yield Unit
 
   // TODO:terminate market则需要将订单从内存中删除,但是不从数据库删除
 
