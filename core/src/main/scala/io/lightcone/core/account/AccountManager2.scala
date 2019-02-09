6feaf5e11dc615406d7e2d3af44fab595bceaf89
Daniel Wang <daniel@loopring.org>
2019-02-09 02:41:46 -0500

    x

---
+++ core/src/main/scala/io/lightcone/core/account/AccountManager2.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.lightcone.core
+
+import scala.concurrent.Future
+
+object AccountManager2 {
+
+  // def default(
+  //   )(
+  //     implicit
+  //     dustEvaluator: DustOrderEvaluator,
+  //     orderPool: AccountOrderPool with UpdatedOrdersTracing
+  //   ): AccountManager2 = new AccountManagerImpl2()
+}
+
+trait AccountManager2 {
+
+  // def setTokenManagerInitializer(f: (String, String) => Future[(BigInt, BigInt)])
+
+  // def hasReserveManager(token: String): Boolean
+  // def addReserveManager(tm: ReserveManager): ReserveManager
+  // def getReserveManager(token: String): ReserveManager
+  // def getOrUpdateReserveManager(tm: ReserveManager): ReserveManager
+
+  def submitOrder(order: Matchable): Future[Boolean]
+
+  // soft cancel an order
+  def cancelOrder(orderId: String): Future[Boolean]
+
+  // hard cancel multiple orders
+  def handleCutoff(cutoff: Long): Future[Int]
+
+  def purgeOrders(marketPair: MarketPair): Future[Int]
+
+  def handleCutoff(
+      cutoff: Long,
+      marketHash: String
+    ): Future[Int]
+
+  def adjustOrder(
+      orderId: String,
+      outstandingAmountS: BigInt
+    ): Future[Boolean]
+
+  //TODO: 需要实现cancelOrdersInMarket与cancelAllOrders
+  //由用户由前端请求，按照address和市场取消订单
+  def cancelOrdersInMarket(marketHash: String): Future[Int]
+
+  def cancelAllOrders(): Future[Int]
+}
