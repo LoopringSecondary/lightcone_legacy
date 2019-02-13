0e5710a86a6fb7d8f9a5d906052b40ed6ef68064
于红雨 <yuhongyu04@126.com>
2019-02-13 16:28:55 +0800

    确认多次提交失败后，订单会被cancel

--- relayer/src/main/scala/io/lightcone/relayer/actors/MarketManagerActor.scala
+++ relayer/src/main/scala/io/lightcone/relayer/actors/MarketManagerActor.scala
@@ -25,6 +25,8 @@ import io.lightcone.relayer.base._
 import io.lightcone.relayer.data._
 import io.lightcone.core._
 import io.lightcone.lib._
+import io.lightcone.persistence.DatabaseModule
+
 import scala.concurrent._
 import scala.concurrent.duration._
 
@@ -46,7 +48,8 @@ object MarketManagerActor extends DeployedAsShardedByMarket {
       rie: RingIncomeEvaluator,
       dustOrderEvaluator: DustOrderEvaluator,
       metadataManager: MetadataManager,
-      deployActorsIgnoringRoles: Boolean
+      deployActorsIgnoringRoles: Boolean,
+      dbModule: DatabaseModule
     ): ActorRef = {
     this.metadataManager = metadataManager
     startSharding(Props(new MarketManagerActor()))
@@ -91,7 +94,8 @@ class MarketManagerActor(
     val tve: TokenValueEvaluator,
     val rie: RingIncomeEvaluator,
     val dustOrderEvaluator: DustOrderEvaluator,
-    val metadataManager: MetadataManager)
+    val metadataManager: MetadataManager,
+    val dbModule: DatabaseModule)
     extends InitializationRetryActor
     with ShardingEntityAware
     with RepeatedJobActor
@@ -161,6 +165,7 @@ class MarketManagerActor(
   protected def settlementActor = actors.get(RingSettlementManagerActor.name)
   protected def metadataRefresher = actors.get(MetadataRefresher.name)
   protected def orderbookManagerActor = actors.get(OrderbookManagerActor.name)
+  protected def mama = actors.get(MultiAccountManagerActor.name)
 
   var gasPrice: BigInt = _
 
@@ -335,6 +340,21 @@ class MarketManagerActor(
 
     if (ou.sells.nonEmpty || ou.buys.nonEmpty) {
       orderbookManagerActor ! ou.copy(marketPair = Some(marketPair))
+    }
+
+    if (matchResult.taker.status.isStatusSoftCancelledTooManyRingFailures) {
+      for {
+        takerOpt <- dbModule.orderService.getOrder(matchResult.taker.id)
+      } yield {
+        takerOpt.foreach { taker =>
+          mama ! CancelOrder.Req(
+            id = taker.hash,
+            owner = taker.owner,
+            status = matchResult.taker.status,
+            marketPair = Some(MarketPair(taker.tokenS, taker.tokenB))
+          )
+        }
+      }
     }
   }
 
