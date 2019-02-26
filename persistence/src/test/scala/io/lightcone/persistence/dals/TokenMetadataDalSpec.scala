cad8bfaa59b1b0ff28744f988666356a19bc3789
dyfvicture <fbi-ceo@163.com>
2019-02-26 17:03:23 +0800

    commit

--- persistence/src/test/scala/io/lightcone/persistence/dals/TokenMetadataDalSpec.scala
+++ persistence/src/test/scala/io/lightcone/persistence/dals/TokenMetadataDalSpec.scala
@@ -35,6 +35,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         address = lrcAddress,
         unit = "T1",
         decimals = 18,
+        precision = 6,
         burnRateForMarket = 0.1,
         burnRateForP2P = 0.1
       ),
@@ -46,6 +47,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         address = "0x222",
         unit = "T2",
         decimals = 18,
+        precision = 6,
         burnRateForMarket = 0.2,
         burnRateForP2P = 0.2
       ),
@@ -57,6 +59,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         address = "0x333",
         unit = "T3",
         decimals = 18,
+        precision = 6,
         burnRateForMarket = 0.3,
         burnRateForP2P = 0.3
       )
@@ -78,12 +81,13 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         lrc.address == lrcAddress &&
         lrc.unit == "T1" &&
         lrc.decimals == 18 &&
+        lrc.precision == 6 &&
         lrc.burnRateForMarket == 0.1 &&
         lrc.burnRateForP2P == 0.1
     )
 
     info("duplicate token address save should return error")
-    val token3 = lrc
+    val token3 = lrc.copy(precision = 8)
     val r3 = dal.saveToken(token3)
     val res3 = Await.result(r3.mapTo[ErrorCode], 5.second)
     assert(res3 == ErrorCode.ERR_PERSISTENCE_DUPLICATE_INSERT)
@@ -129,6 +133,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         bnb.address == "0x222" &&
         bnb.unit == "T2" &&
         bnb.decimals == 18 &&
+        bnb.precision == 6 &&
         bnb.burnRateForMarket == 0.2 &&
         bnb.burnRateForP2P == 0.2
     )
@@ -140,6 +145,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         name = "T2_ Token",
         unit = "T2_",
         decimals = 12,
+        precision = 8,
         burnRateForMarket = 0.5,
         burnRateForP2P = 0.6
       )
@@ -157,6 +163,7 @@ class TokenMetadataDalSpec extends DalSpec[TokenMetadataDal] {
         bnb1.unit == "T2_" &&
         bnb1.address == "0x222" &&
         bnb1.decimals == 12 &&
+        bnb1.precision == 8 &&
         bnb1.burnRateForMarket == 0.5 &&
         bnb1.burnRateForP2P == 0.6
     )
