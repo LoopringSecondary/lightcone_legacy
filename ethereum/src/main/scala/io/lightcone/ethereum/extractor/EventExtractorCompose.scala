c84204977bfd0e58445dfcec3118a3d80e032deb
lyd <lyddream@126.com>
2019-03-01 20:57:28 +0800

    extract events by transaction

--- ethereum/src/main/scala/io/lightcone/ethereum/extractor/EventExtractorCompose.scala
+++ ethereum/src/main/scala/io/lightcone/ethereum/extractor/EventExtractorCompose.scala
@@ -16,7 +16,11 @@
 
 package io.lightcone.ethereum.extractor
 import com.google.inject.Inject
-import io.lightcone.ethereum.event.EventHeader
+import com.google.protobuf.ByteString
+import io.lightcone.core.Amount
+import io.lightcone.ethereum.event.{EventHeader, TransferEvent}
+import io.lightcone.ethereum.{BlockHeader, TxStatus}
+import io.lightcone.lib.{Address, NumericConversion}
 import io.lightcone.relayer.data.{RawBlockData, Transaction, TransactionReceipt}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -46,16 +50,64 @@ class EventExtractorCompose @Inject()(
     this
   }
 
-  def extractEvents(block: RawBlockData): Future[Seq[AnyRef]] =
+  def extractEvents(block: RawBlockData): Future[Seq[Any]] =
     Future.sequence {
       blockExtractors.map(_.extractEvents(block))
-    }
+    }.map(_.flatten)
+
+  def extractEventsByTx(block: RawBlockData): Future[Seq[Any]] = {
+    val header = BlockHeader(
+      hash = block.hash,
+      height = block.height,
+      miner = block.miner,
+      timestamp = NumericConversion.toBigInt(block.timestamp).longValue(),
+      uncles = block.uncles
+    )
+    for {
+      txEvents <- Future
+        .sequence((block.txs zip block.receipts).map {
+          case (tx, receipt) =>
+            val eventHeader = EventHeader(
+              txHash = tx.hash,
+              txStatus =
+                if (NumericConversion.toBigInt(receipt.status) == 1)
+                  TxStatus.TX_STATUS_SUCCESS
+                else TxStatus.TX_STATUS_FAILED,
+              blockHeader = Some(header),
+              txFrom = tx.from,
+              txTo = tx.to,
+              txValue = Some(NumericConversion.toAmount(tx.value))
+            )
+            for {
+              events <- extractEvents(tx, receipt, eventHeader)
+            } yield {
+              if (events.isEmpty)
+                Seq(
+                  TransferEvent(
+                    header = Some(eventHeader),
+                    owner = tx.from,
+                    from = tx.from,
+                    to = tx.to,
+                    token = Address.ZERO.toString(),
+                    amount = Some(
+                      Amount(value = ByteString.copyFrom(BigInt(0).toByteArray))
+                    )
+                  )
+                )
+              else events
+            }
+          case _ => Future.successful(Seq.empty)
+        })
+        .map(_.flatten)
+      blockEvents <- extractEvents(block)
+    } yield txEvents ++ blockEvents
+  }
 
   def extractEvents(
       tx: Transaction,
       receipt: TransactionReceipt,
       eventHeader: EventHeader
-    ): Future[Seq[AnyRef]] = {
+    ): Future[Seq[Any]] = {
     val txData = TransactionData(tx, receipt, eventHeader)
     Future.sequence {
       txExtractors.map(_.extractEvents(txData))
