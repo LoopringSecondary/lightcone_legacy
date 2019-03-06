046c80c4ab1e98ee329be095bc18eab5077e9389
Daniel Wang <dong77@gmail.com>
2019-03-05 19:45:05 -0500

    Optimize Yadong's PR 535 (#537)

--- ethereum/src/main/scala/io/lightcone/ethereum/extractor/EventExtractor.scala
+++ ethereum/src/main/scala/io/lightcone/ethereum/extractor/EventExtractor.scala
@@ -17,12 +17,14 @@
 package io.lightcone.ethereum.extractor
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+import org.slf4s.Logging
 
 trait EventExtractor[S, +E] {
   def extractEvents(source: S): Future[Seq[E]]
 }
 
-object EventExtractor {
+object EventExtractor extends Object with Logging {
 
   // Constructs a new EventExtractor with a given list of sub-EventExtractors and
   // invokes them in the given order. Duplicatd events will be removed.
@@ -33,9 +35,40 @@ object EventExtractor {
       ec: ExecutionContext
     ) = new EventExtractor[S, E] {
 
-    def extractEvents(source: S): Future[Seq[E]] =
+    def extractEvents(source: S): Future[Seq[E]] = {
+
       Future
-        .sequence(extractors.map(_.extractEvents(source)))
+        .sequence(extractors.map(extractAndLog(_, source)))
         .map(_.flatten.distinct)
+    }
+
+    private def extractAndLog(
+        extractor: EventExtractor[S, E],
+        source: S
+      ): Future[Seq[E]] = {
+      val f = extractor.extractEvents(source)
+
+      f.onComplete {
+        case Success(events) =>
+          val size = events.size
+          log.debug(
+            s"EXTRACTOR ${extractor.getClass.getSimpleName} extracted " +
+              s"$size events from: ${source.toString.take(1000)} ===>"
+          )
+
+          events.zipWithIndex foreach {
+            case (evt, idx) => log.debug(s"EVENT ${idx}/${size} --> ${evt}")
+          }
+
+        case Failure(e) =>
+          log.error(
+            s"${extractor.getClass.getSimpleName} failed to extract events from $source",
+            e
+          )
+      }
+
+      f
+    }
+
   }
 }
