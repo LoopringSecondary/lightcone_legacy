e058b88626324d53a72d0d19258dd5c9699eba7f
于红雨 <yuhongyu04@126.com>
2019-03-08 12:01:10 +0800

    modify follow comment

--- relayer/src/main/scala/io/lightcone/relayer/actors/AccountManagerActor.scala
+++ relayer/src/main/scala/io/lightcone/relayer/actors/AccountManagerActor.scala
@@ -63,9 +63,6 @@ class AccountManagerActor(
   import TxStatus._
   import MarketMetadata.Status._
 
-  private var jumpedPendingNonce = Set.empty[Long]
-  private var nonce: Long = 0
-
   val count = KamonSupport.counter("account_manager")
   val timer = KamonSupport.timer("account_manager")
 
@@ -114,20 +111,7 @@ class AccountManagerActor(
 
       val syncCutoff = for {
         res <- (ethereumQueryActor ? batchCutoffReq).mapAs[BatchGetCutoffs.Res]
-        nonceRes <- (activityActor ? GetPendingActivityNonce.Req(
-          owner,
-          pendingTxLength
-        )).mapAs[GetPendingActivityNonce.Res]
       } yield {
-        nonceRes.nonces
-          .sortWith(_ > _)
-          .foreach { n =>
-            if (nonce == 0 || n == nonce + 1) {
-              nonce = n
-            } else {
-              jumpedPendingNonce = jumpedPendingNonce + n
-            }
-          }
         res.resps foreach { cutoffRes =>
           assert(
             cutoffRes.cutoff.get.block >= 0,
@@ -269,7 +253,20 @@ class AccountManagerActor(
 
     case GetAccountNonce.Req(addr) =>
       count.refine("label" -> "get_account_nonce").increment()
-      sender ! GetAccountNonce.Res(nonce)
+      for {
+        nonceFromEth <- (ethereumQueryActor ? GetNonce.Req(owner, "pending"))
+          .mapAs[GetNonce.Res]
+            .map{
+              res => NumericConversion.toBigInt(res.result).longValue()
+            }
+        nonceFromDb <- (activityActor ? GetPendingActivityNonce.Req(owner, pendingTxLength))
+          .mapAs[GetPendingActivityNonce.Res]
+            .map {
+              res => res.nonces.sliding(2).find(s => s(1) - s(0) > 1).map(_(0)+1)
+                    .getOrElse(res.nonces.lastOption.getOrElse(0))
+            }
+        nonce = if (nonceFromEth > nonceFromDb) nonceFromEth else nonceFromDb
+      } yield sender ! GetAccountNonce.Res(nonce)
 
     case req @ CancelOrder.Req("", addr, _, None, _) =>
       count.refine("label" -> "cancel_order").increment()
@@ -317,25 +314,6 @@ class AccountManagerActor(
             }
           }
         } yield result).sendTo(sender)
-      }
-
-    case evt: AddressNonceChangedEvent =>
-      count.refine("label" -> "address_nonce_changed").increment()
-      blocking {
-        Future {
-          assert(evt.from == owner)
-          if (evt.nonce == nonce) {
-            nonce = nonce + 1
-            while (jumpedPendingNonce.contains(nonce)) {
-              jumpedPendingNonce = jumpedPendingNonce - nonce
-              nonce = nonce + 1
-            }
-          } else {
-            if (jumpedPendingNonce.size < 10000) { //限制nonce跳跃的数量不超过10000
-              jumpedPendingNonce = jumpedPendingNonce + evt.nonce
-            }
-          }
-        }
       }
 
     // 为了减少以太坊的查询量，需要每个block汇总后再批量查询，因此不使用TransferEvent
