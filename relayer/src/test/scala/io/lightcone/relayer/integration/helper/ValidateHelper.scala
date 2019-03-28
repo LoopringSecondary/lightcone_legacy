31d5255284c903ca49044bdfd06384cc56aa355d
于红雨 <yuhongyu04@126.com>
2019-03-28 17:26:27 +0800

    mini

--- relayer/src/test/scala/io/lightcone/relayer/integration/helper/ValidateHelper.scala
+++ relayer/src/test/scala/io/lightcone/relayer/integration/helper/ValidateHelper.scala
@@ -43,7 +43,10 @@ trait ValidateHelper {
           .Req(size = 100, marketPair = Some(pair))
           .expectUntil(orderbookMatcher)
         GetUserFills
-          .Req(owner = account.getAddress, marketPair = Some(pair))
+          .Req(
+            owner = account.getAddress,
+            market = Some(MarketFilter(marketPair = Some(pair)))
+          )
           .expectUntil(userFillsMatcher)
         GetMarketFills.Req(Some(pair)).expectUntil(marketFillsMatcher)
     }
