028831ab6ca6ac4ce845ec29c46f9a4507079b3e
lyd <lyddream@126.com>
2019-04-03 11:43:57 +0800

    completely match

--- relayer/src/test/scala/io/lightcone/relayer/integration/matches/MatchesSpec_CompleteMatch.scala
+++ relayer/src/test/scala/io/lightcone/relayer/integration/matches/MatchesSpec_CompleteMatch.scala
@@ -19,16 +19,13 @@ package io.lightcone.relayer.integration.matches
 import io.lightcone.core.OrderStatus._
 import io.lightcone.core._
 import io.lightcone.ethereum.TxStatus
-import io.lightcone.ethereum.event.{
-  EventHeader,
-  OrderFilledEvent,
-  RingMinedEvent
-}
+import io.lightcone.ethereum.event._
 import io.lightcone.relayer.data.AccountBalance.TokenBalance
 import io.lightcone.relayer.data._
 import io.lightcone.relayer.getUniqueAccount
 import io.lightcone.relayer.integration.AddedMatchers._
 import io.lightcone.relayer.integration._
+import io.lightcone.lib.NumericConversion._
 import org.scalatest._
 
 class MatchesSpec_CompleteMatch
@@ -52,6 +49,12 @@ class MatchesSpec_CompleteMatch
         .expectUntil(
           check((res: GetAccount.Res) => res.accountBalance.nonEmpty)
         )
+
+      val initDynamicToken = initAccount.getAccountBalance.tokenBalanceMap(
+        dynamicBaseToken.getAddress()
+      )
+      val initBalance: BigInt = initDynamicToken.getBalance
+      val initAllowance: BigInt = initDynamicToken.getAllowance
 
       When(
         s"account1: ${account1.getAddress} submit an order of sell 100 LRC and set fee to 10 LRC."
@@ -78,6 +81,14 @@ class MatchesSpec_CompleteMatch
       And(
         s"account2: ${account2.getAddress} submit an order of buy 100 LRC and set fee to 10 LRC."
       )
+
+      var submitted = false
+      addSendRawTxExpects({
+        case req: SendRawTransaction.Req => {
+          submitted = true
+          SendRawTransaction.Res()
+        }
+      })
 
       val order2 = createRawOrder(
         tokenB = dynamicBaseToken.getAddress(),
@@ -108,6 +119,15 @@ class MatchesSpec_CompleteMatch
           containsInGetOrdersByHash(STATUS_PENDING, order1.hash, order2.hash)
         )
 
+      Then("send raw transaction to submit ring")
+      val now = timeProvider.getTimeSeconds()
+      while (!submitted && timeProvider
+               .getTimeSeconds() - now < timeout.duration.toSeconds) {
+        Thread.sleep(500)
+      }
+
+      submitted shouldBe true
+
       Then("order book is empty")
       GetOrderbook
         .Req(
@@ -119,6 +139,18 @@ class MatchesSpec_CompleteMatch
         )
 
       Then("dispatch fills and ring mined event")
+
+      addFilledAmountExpects({
+        case req: GetFilledAmount.Req =>
+          GetFilledAmount.Res(
+            filledAmountSMap = (req.orderIds map { id =>
+              if (id == order1.hash)
+                id -> order1.getAmountS
+              else if (id == order2.hash) id -> order2.getAmountS
+              else id -> toAmount(BigInt(0))
+            }).toMap
+          )
+      })
 
       val txHash =
         "0x19e575bfe3671b54d70fea96aa96e1c4f133e39f07b31c1f2f0fb71e61c4f84a"
@@ -145,11 +177,94 @@ class MatchesSpec_CompleteMatch
         orderIds = Seq(order1.hash, order2.hash),
         marketPair = Some(dynamicMarketPair)
       )
-
       eventDispatcher.dispatch(fill1)
       eventDispatcher.dispatch(fill2)
       eventDispatcher.dispatch(ringMinedEvent)
 
+      val addressBalanceUpdatedEvent1 = AddressBalanceUpdatedEvent(
+        address = account1.getAddress,
+        token = dynamicBaseToken.getAddress(),
+        balance = initBalance - order1.getAmountS - order1.getFeeParams.getAmountFee,
+        block = 1L
+      )
+
+      val addressAllowanceUpdatedEvent1 = AddressAllowanceUpdatedEvent(
+        address = account1.getAddress,
+        token = dynamicBaseToken.getAddress(),
+        allowance = initAllowance - order1.getAmountS - order1.getFeeParams.getAmountFee,
+        block = 1L
+      )
+
+      eventDispatcher.dispatch(addressAllowanceUpdatedEvent1)
+      eventDispatcher.dispatch(addressBalanceUpdatedEvent1)
+
+      val addressBalanceUpdatedEvent2 = AddressBalanceUpdatedEvent(
+        address = account2.getAddress,
+        token = dynamicBaseToken.getAddress(),
+        balance = initBalance + order2.getAmountB - order2.getFeeParams.getAmountFee,
+        block = 1L
+      )
+      eventDispatcher.dispatch(addressBalanceUpdatedEvent2)
+
+      Then("order1 and order2 are STATUS_COMPLETELY_FILLED")
+      GetOrdersByHash
+        .Req(
+          hashes = Seq(order1.hash, order2.hash)
+        )
+        .expectUntil(
+          containsInGetOrdersByHash(
+            STATUS_COMPLETELY_FILLED,
+            order1.hash,
+            order2.hash
+          )
+        )
+
+      Then("order book is empty")
+      GetOrderbook
+        .Req(
+          size = 10,
+          marketPair = Some(dynamicMarketPair)
+        )
+        .expect(
+          orderBookIsEmpty()
+        )
+
+      Then(s"${account1.getAddress} and ${account2.getAddress} are updated")
+      GetAccount
+        .Req(
+          address = account1.getAddress,
+          allTokens = true
+        )
+        .expectUntil(
+          accountBalanceMatcher(
+            token = dynamicBaseToken.getAddress(),
+            tokenBalance = TokenBalance(
+              token = dynamicBaseToken.getAddress(),
+              balance = initBalance - order1.getAmountS - order1.getFeeParams.getAmountFee,
+              allowance = initAllowance - order1.getAmountS - order1.getFeeParams.getAmountFee,
+              availableBalance = initBalance - order1.getAmountS - order1.getFeeParams.getAmountFee,
+              availableAlloawnce = initAllowance - order1.getAmountS - order1.getFeeParams.getAmountFee
+            )
+          )
+        )
+
+      GetAccount
+        .Req(
+          address = account2.getAddress,
+          allTokens = true
+        )
+        .expectUntil(
+          accountBalanceMatcher(
+            token = dynamicBaseToken.getAddress(),
+            tokenBalance = TokenBalance(
+              token = dynamicBaseToken.getAddress(),
+              balance = initBalance + order2.getAmountB - order2.getFeeParams.getAmountFee,
+              allowance = initAllowance,
+              availableBalance = initBalance + order2.getAmountB - order2.getFeeParams.getAmountFee,
+              availableAlloawnce = initAllowance
+            )
+          )
+        )
 
     }
   }
