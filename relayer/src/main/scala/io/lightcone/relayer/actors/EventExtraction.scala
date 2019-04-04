07cc09b25633a362512edf6ad761abc7a9ac3f29
hongyu <yuhongyu04@126.com>
2019-04-04 12:19:02 +0800

    MissingBlock中不发送BlockEvent (#609)
    * mini
    
    * mini
    
    * mini

--- relayer/src/main/scala/io/lightcone/relayer/actors/EventExtraction.scala
+++ relayer/src/main/scala/io/lightcone/relayer/actors/EventExtraction.scala
@@ -66,21 +66,6 @@ trait EventExtraction {
                 .toBigInt(blockData.number) == -1) {
             preBlockData = blockData
             blockData = block
-            val blockEvent = BlockEvent(
-              blockNumber =
-                NumericConversion.toBigInt(blockData.number).longValue(),
-              txs = blockData.transactions.map(
-                tx =>
-                  BlockEvent.Tx(
-                    from = Address.normalize(tx.from),
-                    nonce = NumericConversion.toBigInt(tx.nonce).toInt,
-                    txHash = tx.hash
-                  )
-              )
-            )
-
-            //TODO: 如何发送，是否需要等待返回结果之后再发送其余的events，否则会导致数据不一致
-            eventDispatcher.dispatch(blockEvent)
             self ! RETRIEVE_RECEIPTS
           } else {
             self ! BLOCK_REORG_DETECTED
@@ -151,8 +136,13 @@ trait EventExtraction {
 
   def processEvents: Future[Unit] = {
     for {
-      events <- eventExtractor.extractEvents(blockData)
-      _ = events.foreach(eventDispatcher.dispatch)
+      events_ <- eventExtractor.extractEvents(blockData)
+      events = if (unSupportedEvents.isEmpty) events_
+      else events_.filterNot(evt => unSupportedEvents.contains(evt.getClass))
+      (blockEvent, otherEvents) = events.partition(_.isInstanceOf[BlockEvent])
+      //TODO: 首先发送BlockEvent，如何发送，是否需要等待返回结果之后再发送其余的events，否则会导致数据不一致
+      _ = blockEvent.foreach(eventDispatcher.dispatch)
+      _ = otherEvents.foreach(eventDispatcher.dispatch)
       _ <- dbModule.blockService.saveBlock(
         BlockData(
           hash = blockData.hash,
@@ -165,5 +155,8 @@ trait EventExtraction {
   }
 
   def postProcessEvents(): Future[Unit] = Future.unit
+
+  def unSupportedEvents: Seq[Class[_]] =
+    Seq.empty //MissingBlocksEventExtractorActor与EthereumEventExtractorActor需要处理的事件不一致，有些事件不需要Miss的发送
 
 }
